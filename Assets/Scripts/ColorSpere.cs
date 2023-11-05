42721d73ffc966537fea21f7b5bd4362aad8c8ab
arzutepe <38106558+arzutp@users.noreply.github.com>
2023-11-05 19:10:07 +0300

    collider

--- Assets/Scripts/ColorSpere.cs
+++ Assets/Scripts/ColorSpere.cs
@@ -6,19 +6,35 @@ using UnityEngine;
 
 public class ColorSpere : MonoBehaviour
 {
+    public LayerMask layer;
+    [SerializeField] List<ColorSpere> neightboorColorSpere = new List<ColorSpere>();
+    public bool inLevel;
     [SerializeField] Rigidbody spereRB;
     [SerializeField] Transform parent;
-    public SpereColor spereColor;
+    SpereColor color;
     public Renderer spereRenderer;
     public bool spereIsFire;
     int enumCount;
     int rand;
     float speed = 7f;
+    [SerializeField]
+    private float checkRadius;
     void Start()
     {
+        if (inLevel)
+            FindNeightbor();
         enumCount = Enum.GetValues(typeof(SpereColor)).Length;
         rand = UnityEngine.Random.Range(0, enumCount);
         ColorChange((SpereColor)rand);
+    }
+
+    private void SetPosition()
+    {
+        Vector3 target = parent.position;
+        Vector3 pos = transform.position;
+        Vector3 norm = target - pos;
+        Vector3 force = norm.normalized * speed;
+        spereRB.AddForce(force);
     }
 
     public void ColorChange(SpereColor spereColor)
@@ -27,15 +43,19 @@ public class ColorSpere : MonoBehaviour
         {
             case SpereColor.Red:
                 spereRenderer.material.color = Color.red;
+                color = SpereColor.Red;
                 break;
             case SpereColor.Blue:
                 spereRenderer.material.color = Color.blue;
+                color = SpereColor.Blue;
                 break;
             case SpereColor.Green:
                 spereRenderer.material.color = Color.green;
+                color = SpereColor.Green;
                 break;
             case SpereColor.Yellow:
                 spereRenderer.material.color = Color.yellow;
+                color = SpereColor.Yellow;
                 break;
             default:
                 Debug.LogWarning("Geçersiz renk seçimi");
@@ -52,9 +72,42 @@ public class ColorSpere : MonoBehaviour
         }
     }
 
+    Ray ray;
+    public float spereCastRadius = 0.1f;
+    Collider[] hits;
+    public void FindNeightbor()
+    {
+        layer = LayerMask.NameToLayer("ColorSpere");
+
+        hits = Physics.OverlapSphere(transform.position, transform.localScale.x * .5f);
+        if (hits.Length > 0)
+        {
+            foreach (var item in hits)
+            {
+
+                if (item.gameObject.name != parent.gameObject.name && item.gameObject.name != this.gameObject.name)
+                {
+                    if (!neightboorColorSpere.Contains(item.transform.GetComponent<ColorSpere>()))
+                    {
+                        neightboorColorSpere.Add(item.transform.GetComponent<ColorSpere>());
+
+                    }
+                }
+            }
+        }
+        //{
+        //    neightboorColorSpere.Add(hit.transform.GetComponent<ColorSpere>());
+        //    print(hit.transform.GetComponent<ColorSpere>().color + " spere color collider");
+        //}
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
+        //if (collision.transform.CompareTag("ColorSpere"))
+        //{
+        //    neightboorColorSpere.Add(collision.transform.GetComponent<ColorSpere>());   
+        //    print(collision.transform.GetComponent<ColorSpere>().color + " spere color collider");
+        //}
         if (spereIsFire)
         {
             if (collision.transform.CompareTag("ColorSpere"))
@@ -63,6 +116,7 @@ public class ColorSpere : MonoBehaviour
                 spereRB.velocity = Vector3.zero;
                 spereRB.constraints = RigidbodyConstraints.FreezeAll;
                 this.transform.SetParent(GameManager.Instance.SpereParentTransform());
+                FindNeightbor();
             }
         }
     }
